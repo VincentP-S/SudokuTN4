f4fb406294e7918e9a39b26aaee8c1c6ac8f8985
VincentP-S <65359170+VincentP-S@users.noreply.github.com>
2021-08-14 19:33:59 -0400

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+        reconstruire grille sudoku 
+        charger a partir d'un fichier .txt
+        manipuler la grille (placements verifications) ?????
+        reproduire la grille a l'ecran ou dans un autre fichier
+        triplet:
+        x ligne 
+        y colonne
+        z valeur 
+        
+
+        partie 2 erronee et sans retour de chariot
+
+
+        1. charger  un fichier (online ou sur disque ou stdin) ** gerer les erreurs de lecture
+        2. valider si la grille Sudoku est valable (9x9 / 1 fois par rangee et 1 fois par colonne) **generer une exception
+        3. transposition au tableau correspondant à un calcul de matrice transposée et afficher le résultat à l’écran
+
+        tester avec plusieurs grilles inclure des grilles asymetrique
+        Donner le resultat de 2 tests differents (screenshot ok pour le resultats de ces tests)
+
+        expliquer tout (fn var et qualifiants)
+
+        Je test la connection avec GitHub
+        */
+    }
+}
